2fd3b2f239cb5b439c75b80298f40cd8c6b3d3d9
Erick Maia <erickmaia134@gmail.com>
2025-05-24 15:05:16 -0300

    MongoDB (README com o Mongo)

---
+++ src/main/java/org/example/expedfacil/mongo/LogService.java
@@ -0,0 +1,23 @@
+package org.example.expedfacil.mongo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class LogService {
+
+    @Autowired
+    private LogDeCargaRepository repository;
+
+    public void registrarLog(String cargaId, String msg){
+        LogDeCarga log = new LogDeCarga();
+
+        log.setCargaId(cargaId);
+        log.setMensagem(msg);
+        log.setData(LocalDateTime.now().toString());
+        repository.save(log);
+
+    }
+}
