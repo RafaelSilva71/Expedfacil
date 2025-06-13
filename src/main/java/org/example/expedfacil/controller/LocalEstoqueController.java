13285d5dd24ed77ddb66e3bae50a92d1ed5d6935
ricardoissadeousa <ricardoissadesousa@gmail.com>
2025-06-13 09:53:07 -0300

    Finalizado o swagger da primeira parte

--- src/main/java/org/example/expedfacil/controller/LocalEstoqueController.java
+++ src/main/java/org/example/expedfacil/controller/LocalEstoqueController.java
@@ -1,12 +1,14 @@
 package org.example.expedfacil.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.expedfacil.model.Carga;
 import org.example.expedfacil.model.LocalEstoqueProduto;
+import org.example.expedfacil.model.Produto;
 import org.example.expedfacil.repository.CargaRepository;
 import org.example.expedfacil.repository.LocalEstoqueProdutoRepository;
 import org.example.expedfacil.service.CargaService;
@@ -24,13 +26,58 @@ public class LocalEstoqueController {
 
     @Autowired
     private LocalEstoqueProdutoRepository localEstoqueProdutoRepository;
+    @Operation(
+            summary = "Recupera todos os recusos de produto",
+            description = "Recurso para recuperar um todos os dados de produto",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Recurso recuperado com sucesso",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = LocalEstoqueProduto.class))
 
+                            )
+                    ),
+
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Erro interno do servidor!",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class)
+                            )
+                    )
+            }
+    )
     // Listar todos os locais de estoque salvos
     @GetMapping
     public ResponseEntity<List<LocalEstoqueProduto>> listarTodos() {
         List<LocalEstoqueProduto> todos = localEstoqueProdutoRepository.findAll();
         return ResponseEntity.ok(todos);
     }
+    @Operation(
+            summary = "Lista locais de estoque por número de embarque da carga",
+            description = "Recupera todos os locais de estoque de produtos associados a um número de embarque de carga específico.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Lista de locais de estoque de produtos recuperada com sucesso .",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = LocalEstoqueProduto.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Erro interno do servidor",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class)
+                            )
+                    )
+            }
+    )
 
     // Listar locais de uma carga específica
     @GetMapping("/carga/{numeroEmbarque}")
@@ -59,7 +106,7 @@ public class LocalEstoqueController {
                                     schema = @Schema(implementation = ErrorMessage.class)
                             )
                     ),
-                    @ApiResponse( // Opcional, para casos onde a string enviada poderia ser inválida para seu serviço
+                    @ApiResponse(
                             responseCode = "400",
                             description = "Requisição inválida (ex: formato incorreto do local de estoque)",
                             content = @Content(
--- src/main/java/org/example/expedfacil/mongo/ArquivoController.java
+++ src/main/java/org/example/expedfacil/mongo/ArquivoController.java
@@ -1,5 +1,13 @@
 package org.example.expedfacil.mongo;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.example.expedfacil.controller.dto.CreateProdutoDTO;
+import org.example.expedfacil.model.Produto;
+import org.springdoc.api.ErrorMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.mongodb.gridfs.GridFsResource;
@@ -7,7 +15,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
+@Tag(name ="Arquivos",
+        description="Contém todas as operações relativas aos recursos para o Arquivos contem o CRUD, Criado Especificamente para salvar imagens ! ")
 @RestController
 @RequestMapping("/arquivo")
 public class ArquivoController {
@@ -15,6 +24,36 @@ public class ArquivoController {
     @Autowired
     private ArquivoService arquivoService;
 
+    @Operation(
+            summary = "Cria um novo Arquivo",
+            description = "Recurso para criar um Novo Arquivo",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Recurso criado com sucesso",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "409",
+                            description = "Arquivo já cadastrado!",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "422",
+                            description = "Recurso não processado por dados de entrada inválidos",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class)
+                            )
+                    )
+            }
+    )
     @PostMapping("/upload")
     public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file,
                                          @RequestParam(value = "descricao", required = false) String descricao) {
@@ -25,6 +64,32 @@ public class ArquivoController {
             return ResponseEntity.status(500).body("Erro ao salvar: " + e.getMessage());
         }
     }
+    @Operation(
+            summary = "Recupera um recurso do Arquivo por ID",
+            description = "Retorna um arquivo associado a um arquvo a partir do ID fornecido.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Arquivo recuperado com sucesso",
+                            content = @Content(
+                                    mediaType = "application/octet-stream",
+                                    schema = @Schema(type = "string", format = "binary")
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Recurso não encontrado",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Erro interno ao buscar o arquivo"
+                    )
+            }
+    )
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getArquivo(@PathVariable String id) {
