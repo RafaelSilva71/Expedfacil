d1d9556f4749b63877232241f197ca9e86db9c2a
RafaelSilva <rafaelsilva.orizona@gmail.com>
2025-06-15 15:06:50 -0300

    fix local entidade log de carga

--- src/main/java/org/example/expedfacil/mongo/LogDeCargaRepository.java
+++ src/main/java/org/example/expedfacil/mongo/LogDeCargaRepository.java
@@ -1,5 +1,6 @@
 package org.example.expedfacil.mongo;
 
+import org.example.expedfacil.model.LogDeCarga;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface LogDeCargaRepository extends MongoRepository<LogDeCarga, String> {
